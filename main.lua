d2a414b259c1c2d211eb6ce8b306f19f897de731
Thecivilizations <1312734060@qq.com>
2017-08-10 21:56:24 +0800

    MapTest

--- main.lua
+++ main.lua
@@ -5,15 +5,14 @@ io.stdout:setvbuf("no") --控制台输出窗口,优先输出如果没有则在�
 	项目主体部分
 	
 --]]
+local Map = {}
+Map.width,Map.height,Map.background,Map.Tiles,Map.hiddenTiles = require("Maps/Map")
+local include = {}
 
 
 
 
 function love.load() --资源加载回调函数，仅初始化时调用一次
-
-
-
-    
 
 end
 
@@ -56,5 +55,17 @@ function love.mousepressed(key,x,y) --回调函数释放鼠标按钮时触发。
 
 
 
+end
+
+function analysis(m)
+	local tab = m
+	local result = {}
+	for k,v in pairs(m) do
+		if #result>0 and v~= 0 then
+			for i,j in pairs(result) do
+
+			end
+		end
+	end
 end
 
