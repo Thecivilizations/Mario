db3dfe57079ff98a4c446a2ce18c05f4adfd37cf
Thecivilizations <1312734060@qq.com>
2017-08-10 21:40:27 +0800

    Initial commit

---
+++ main.lua
@@ -0,0 +1,60 @@
+
+io.stdout:setvbuf("no") --控制台输出窗口,优先输出如果没有则在程序结束后再输出的
+
+--[[ 
+	项目主体部分
+	
+--]]
+
+
+
+
+function love.load() --资源加载回调函数，仅初始化时调用一次
+
+
+
+    
+
+end
+
+
+
+function love.update(dt) --更新回调函数，每周期调用
+
+
+
+
+end
+
+
+
+
+function love.draw() --绘图回调函数，每周期调用
+
+
+
+    
+
+
+end
+
+
+
+function love.keypressed(key) --键盘检测回调函数，当键盘事件触发是调用
+
+
+
+
+
+end
+
+
+
+function love.mousepressed(key,x,y) --回调函数释放鼠标按钮时触发。
+
+
+
+
+
+end
+
